14c1788460b35f2074f0f0336a671deccab89ef2
Tahzeebsattar <mc100200812@vu.edu.pk>
2017-08-22 19:45:54 -0700

    Added Comic Books Controler

---
+++ src/Comic-book-gellary/Controllers/ComicBookController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comic_book_gellary.Controllers
+{
+    public class ComicBookController : Controller
+    {
+
+    }
+}
