ab43c9e5417c0a518106a0d4e11d6502736cf726
Tahzeebsattar <mc100200812@vu.edu.pk>
2017-08-22 19:51:14 -0700

    Added Details Function

--- src/Comic-book-gellary/Controllers/ComicBookController.cs
+++ src/Comic-book-gellary/Controllers/ComicBookController.cs
@@ -8,6 +8,9 @@ namespace Comic_book_gellary.Controllers
 {
     public class ComicBookController : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from commic books controller";
+        }
     }
 }
