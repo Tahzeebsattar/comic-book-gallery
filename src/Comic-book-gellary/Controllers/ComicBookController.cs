fd1748d27c95d599dc9238aeb6f70861d97077f5
Tahzeebsattar <mc100200812@vu.edu.pk>
2017-08-27 21:40:26 -0700

    updated commic book detail view

--- src/Comic-book-gellary/Controllers/ComicBookController.cs
+++ src/Comic-book-gellary/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comic_book_gellary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,24 @@ namespace Comic_book_gellary.Controllers
     {
        public ActionResult Detail ()
         {
-           ViewBag.SeriesTitle = "The Amazing Spiderman";
-           ViewBag.IssueNumber = 700;
-           ViewBag.Description = "<p>The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.</p>";
-           ViewBag.Artists = new string[]
-           {
-               "Script: Dan Slott",
-               "Pencils: Humberto Ramos",
-               "Inks: Victor Olazaba",
-               "Colors: Edgar Delgado",
-               "Letters: Chris Eliopoulos"
-           };
+            var commicBook = new CommicBook() 
+            {
+                SeriesTitle = "The Amazing Spiderman",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.</p>",
+                Artists = new Artist[] 
+                { 
+                    new Artist() {Name = "Dan Slott", Role = "Script"},
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"}
+                }
+                
+            };
 
-            return View();
+           return View(commicBook);
+
         }
     }
 }
