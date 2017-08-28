3ccf50ec88817187a27ab792b354c72790e3baa9
Tahzeebsattar <mc100200812@vu.edu.pk>
2017-08-27 18:39:39 -0700

    Ok

--- src/Comic-book-gellary/Controllers/ComicBookController.cs
+++ src/Comic-book-gellary/Controllers/ComicBookController.cs
@@ -8,12 +8,21 @@ namespace Comic_book_gellary.Controllers
 {
     public class ComicBookController : Controller
     {
-       public ContentResult Detail ()
+       public ActionResult Detail ()
         {
-            return new ContentResult()
-            {
-                Content = "Hello from Content Result"
-            };
+           ViewBag.SeriesTitle = "The Amazing Spiderman";
+           ViewBag.IssueNumber = 700;
+           ViewBag.Description = "<p>The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.The quick brown fox jumps right over the lazy dog.</p>";
+           ViewBag.Artists = new string[]
+           {
+               "Script: Dan Slott",
+               "Pencils: Humberto Ramos",
+               "Inks: Victor Olazaba",
+               "Colors: Edgar Delgado",
+               "Letters: Chris Eliopoulos"
+           };
+
+            return View();
         }
     }
 }
